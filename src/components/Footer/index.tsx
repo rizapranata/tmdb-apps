e61e4ec0e31f4f15dbf84bc698563cbb732f2c2b
Riza Pranata <corodotc@gmail.com>
2025-06-05 19:36:03 +0700

    update footer wording

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="w-full md:flex md:justify-around lg:flex lg:justify-around text-sm bg-slate-700 mx-auto text-center text-white py-4">
-        <p>&copy; 2023 Movie App</p>
+        <p>&copy; 2025 Movie App - ❄rizapranata</p>
         <p>All rights reserved.</p>
         <p>Powered by React and Redux</p>
       </div>
