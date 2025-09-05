b63a157e3e5beced2b5e0e8549f01c6610389cd8
Riza Pranata <corodotc@gmail.com>
2025-09-05 14:44:38 +0700

    centering movie time icon on footer section

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,10 @@ import movieIconGrey from "../../assets/images/MoovieTime-Logo-Grey.svg";
 export default function Footer() {
   return (
     <footer className="footer w-full">
-      <div className=" bg-slate-700 ">
-        <div className="container md:flex md:justify-around text-sm text-center text-gray-400 py-4">
+      <div className="bg-slate-700">
+        <div className="container justify-items-center md:flex md:justify-around text-sm text-gray-400 py-4">
           <p>&copy; 2025 Movie App - ❄rizapranata</p>
-          <img className="w-20" src={movieIconGrey} alt="movie-icon" />
+          <img className="w-20 py-3 md:py-0" src={movieIconGrey} alt="movie-icon" />
           <p>Powered by React and Redux</p>
         </div>
       </div>
